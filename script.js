7ea18132d3794faefc1e5d66ba44ebd80b1a933f
Wilder <w.cossio.r@gmail.com>
2023-09-07 16:08:38 -0400

    getting all labels from the login form

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-let emailLabel = document.querySelector('.email-container > label');
-let passLabel = document.querySelector('.pass-container > label');
-let emailInput = document.querySelector('#email');
-let passInput = document.querySelector('#password');
+// let emailLabel = document.querySelector('.email-container > label');
+// let passLabel = document.querySelector('.pass-container > label');
+// let emailInput = document.querySelector('#email');
+// let passInput = document.querySelector('#password');
 
-emailInput.addEventListener('focus', () => {
-    emailLabel.style.bottom = '30px';
-});
+// emailInput.addEventListener('focus', () => {
+//     emailLabel.style.bottom = '30px';
+// });
 
-emailInput.addEventListener('blur', () => {
-    emailLabel.style.bottom = '10px';
-});
+// emailInput.addEventListener('blur', () => {
+//     emailLabel.style.bottom = '10px';
+// });
 
-passInput.addEventListener('focus', () => {
-    passLabel.style.bottom = '30px';
-});
+// passInput.addEventListener('focus', () => {
+//     passLabel.style.bottom = '30px';
+// });
 
-passInput.addEventListener('blur', () => {
-    passLabel.style.bottom = '10px';
-});
+// passInput.addEventListener('blur', () => {
+//     passLabel.style.bottom = '10px';
+// });
+
+let labels = document.querySelectorAll('.login label');
