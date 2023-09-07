eb98db525ba854bc6234afddd5f5e9948fcbfe6f
Wilder <w.cossio.r@gmail.com>
2023-09-07 17:54:28 -0400

    adding the wave effect to every label

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-// let emailLabel = document.querySelector('.email-container > label');
-// let passLabel = document.querySelector('.pass-container > label');
-// let emailInput = document.querySelector('#email');
-// let passInput = document.querySelector('#password');
-
-// emailInput.addEventListener('focus', () => {
-//     emailLabel.style.bottom = '30px';
-// });
-
-// emailInput.addEventListener('blur', () => {
-//     emailLabel.style.bottom = '10px';
-// });
-
-// passInput.addEventListener('focus', () => {
-//     passLabel.style.bottom = '30px';
-// });
-
-// passInput.addEventListener('blur', () => {
-//     passLabel.style.bottom = '10px';
-// });
 
 let labels = document.querySelectorAll('.login label');
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, index) => `<span style="transition-delay: ${index * 50}ms">${letter}</span>`)
+        .join('');
+});
